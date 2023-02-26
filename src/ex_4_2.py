c867864e0da7cf8e714b9a3ed659e35401e9fcc5
supriya-gadiparthi <123222739+supriya-gadiparthi@users.noreply.github.com>
2023-02-25 21:13:13 -0600

    Update ex_4_2.py

--- src/ex_4_2.py
+++ src/ex_4_2.py
@@ -3,18 +3,15 @@ from datetime import datetime
 
 
 def logstamp_to_datetime(datestr):
-    """
-    Your docstring here.  Replace the pass keyword below with your implementation.
-    """
-        
-    format = '%Y-%m-%dT%H:%M:%S'
-    
-    date_ = datetime.strptime(datestr, format)
-    
-    return date_
+    try:
+        # Try to parse the date string as-is
+        return datetime.strptime(datestr, '%Y-%m-%dT%H:%M:%S')
+    except ValueError:
+        # If parsing fails, try to add microseconds and parse again
+        return datetime.strptime(datestr + '.000000', '%Y-%m-%dT%H:%M:%S.%f')
 
 
 # >>>> The code below will call your function and print the results
-if _name_ == "_main_":
+if __name__ == "__main__":
     test_date = '2022-12-01T01:02:03'
     print(f'{logstamp_to_datetime(test_date)=}')
