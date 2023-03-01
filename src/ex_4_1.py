a2884e1a0793310771edfd4490d3b4d325bc7b19
supriya-gadiparthi <123222739+supriya-gadiparthi@users.noreply.github.com>
2023-02-28 21:18:02 -0600

    Update ex_4_1.py

--- src/ex_4_1.py
+++ src/ex_4_1.py
@@ -1,23 +1,22 @@
 """ ex_4_1.py """
-import os
+def get_shutdown_events(logfile):
+    """ Function 1"""
+    f = open(logfile, 'rt')
+    x = list()
 
-try:
-    from src.ex_4_0 import get_shutdown_events
-    from src.util import get_data_file_path
-except ImportError:
-    from ex_4_0 import get_shutdown_events
-    from util import get_data_file_path
+    for i in f:
+        x.append(i)
 
-# Use this FILENAME variable to test your function.
-FILENAME = get_data_file_path('messages.log')
-# >>>> DO NOT MODIFY CODE ABOVE <<<<
+    final_lis = list()
+
+    for i in x:
+        if i.split()[4] == 'initiated.':
+            final_lis.append(i[:-2])
+
+    return final_lis
 
 
 def num_shutdowns(logfile):
-    shutdown_events = get_shutdown_events(logfile)
-    return len(shutdown_events)
-
-
-# >>>> The code below will call your function and print the results
-if __name__ == "__main__":
-    print(f'{num_shutdowns(FILENAME)=}')
+    """ Function 2"""
+    x = get_shutdown_events(logfile)
+    return len(x)
